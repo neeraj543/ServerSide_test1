13abe84b6d3053ee2930e7319bc6d4dc93c52dfe
Neeraj <r1008394@student.thomasmore.be>
2024-12-11 10:11:06 +0100

    Library data, repository

--- src/main/java/be/thomasmore/bookserver/repositories/LibraryRepository.java
+++ src/main/java/be/thomasmore/bookserver/repositories/LibraryRepository.java
@@ -1,11 +1,13 @@
 package be.thomasmore.bookserver.repositories;
 
+import be.thomasmore.bookserver.model.Book;
 import be.thomasmore.bookserver.model.Library;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.lang.NonNull;
 
 import java.util.List;
 
-public interface LibraryRepository {
+public interface LibraryRepository  extends CrudRepository<Library, Integer> {
         @NonNull
         List<Library> findAll();
 }
